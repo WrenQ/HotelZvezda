9ad3e600ee8e137ea1886c3514fc8040e086798e
Enrique <enrique.arauz@gmail.com>
2015-05-07 19:59:01 +0200

    Views modification. Room available finding by hotel not functional yet.

--- src/main/java/iw/zvezdahotels/web/HotelController.java
+++ src/main/java/iw/zvezdahotels/web/HotelController.java
@@ -33,15 +33,13 @@ public class HotelController {
 			@RequestParam("dateLeaving") @DateTimeFormat(iso = ISO.DATE) Date dateLeaving,
 			@RequestParam("hotelId") long hotelId, @RequestParam("categoryId") long categoryId, @RequestParam("roomType") RoomType roomType,
 			@RequestParam(required = false, defaultValue = "false") Boolean extraBed, Model uiModel){
-		Long availableRoomsN = Hotel.findHotelsByAvailableRooms(hotelId, categoryId, roomType).getSingleResult() -
-				Hotel.findHotelsByBookings(hotelId, categoryId, roomType, dateArrival, dateLeaving).getSingleResult();
+		Long availableRoomsN = Hotel.findHotelsByAvailableRooms(hotelId, categoryId, roomType).getSingleResult();
 		if(availableRoomsN > 0){
 			uiModel.addAttribute("category", Category.findCategory(categoryId).getCName());
 			uiModel.addAttribute("roomtype", roomType.name());
 			uiModel.addAttribute("availableRooms", availableRoomsN);
 		} else if (roomType.name().equals("SINGLE")){
-			Long availableRoomsN2 = Hotel.findHotelsByAvailableRooms(hotelId, categoryId, RoomType.DOBLE).getSingleResult() -
-					Hotel.findHotelsByBookings(hotelId, categoryId, roomType, dateArrival, dateLeaving).getSingleResult();
+			Long availableRoomsN2 = Hotel.findHotelsByAvailableRooms(hotelId, categoryId, RoomType.DOBLE).getSingleResult();
 			if(availableRoomsN2 > 0){
 				uiModel.addAttribute("doubleAvailable", true);
 				uiModel.addAttribute("category", Category.findCategory(categoryId).getCName());
@@ -68,8 +66,5 @@ public class HotelController {
         return price;
     }
 	
-	private void populateFormEdit(Model uiModel, Hotel hotel){
-		
-	}
 	
 }
